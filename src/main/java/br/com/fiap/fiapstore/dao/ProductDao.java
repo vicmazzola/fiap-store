4b533d318981eb4d05cc83d420b948b81d9339ed
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-04 07:19:47 -0300

    config dao interface

---
+++ src/main/java/br/com/fiap/fiapstore/dao/ProductDao.java
@@ -0,0 +1,18 @@
+package br.com.fiap.fiapstore.dao;
+import br.com.fiap.fiapstore.exception.DBException;
+import br.com.fiap.fiapstore.model.Product;
+import java.util.List;
+
+
+public class ProductDao {
+
+    void register(Product product) throws DBException;
+    void update(Product product) throws DBException;
+    void remove(Product product);
+    Product search(int id);
+    List<Product> list();
+
+
+
+}
+
